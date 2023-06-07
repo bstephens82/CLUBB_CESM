aeb71970d8193d26c4b7de167286735e6a7a6ac7
bstephens82 <benjamin.andrew.stephens@gmail.com>
2023-06-07 10:03:35 -0600

    Fixing oversight.

--- advance_xm_wpxp_module.F90
+++ advance_xm_wpxp_module.F90
@@ -2531,8 +2531,8 @@ module advance_xm_wpxp_module
 
           ! Only compute the Coriolis term if the model is running on its own,
           ! and is not part of a larger, host model.
-          um_tndcy = um_forcing !- fcor * ( vg - vm )
-          vm_tndcy = vm_forcing !+ fcor * ( ug - um )
+          um_tndcy = um_forcing - fcor * ( vg - vm )
+          vm_tndcy = vm_forcing + fcor * ( ug - um )
 
           if ( l_stats_samp ) then
 
